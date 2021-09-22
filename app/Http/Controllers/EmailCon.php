500e6150e4c1da2a103d9f7598ce40de7f0b35ed
microzool <microzool2020@gmail.com>
2021-09-22 08:30:15 -0700

    first commit

--- app/Http/Controllers/EmailCon.php
+++ app/Http/Controllers/EmailCon.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class EmailCon extends Controller
 {
     //
+
+    public function index(){
+        
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'email' => 'required|unique:emails,email|max:255',
+           
+        ]);
+    }
 }
