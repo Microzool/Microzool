2206a9af35e5edbafe3312de7afffeea78df560f
microzool <microzool2020@gmail.com>
2021-09-22 08:35:03 -0700

    Initial commit

--- app/Http/Controllers/EmailCon.php
+++ app/Http/Controllers/EmailCon.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Email;
 class EmailCon extends Controller
 {
     //
@@ -16,6 +16,14 @@ class EmailCon extends Controller
         $validated = $request->validate([
             'email' => 'required|unique:emails,email|max:255',
            
-        ]);
+        ],[
+
+
+        ]
+    
+    );
+
+    Email::create($request->all());
+    
     }
 }
